76109af0b3d07eb1f6fefcff89c258f368c92b63
Pele98 <pekka.lehtola98@gmail.com>
2022-09-25 13:15:34 +0300

    Added DateTime scalar to note.

--- assingment1/resolvers/index.cjs
+++ assingment1/resolvers/index.cjs
@@ -1,7 +1,9 @@
 const Query = require('./query.cjs');
 const Mutation = require('./mutation');
+const { GraphQLDateTime } = require('graphql-scalars');
 
 module.exports = {
   Query,
-  Mutation
+  Mutation,
+  DateTime: GraphQLDateTime
 };
--- assingment1/schema.cjs
+++ assingment1/schema.cjs
@@ -1,11 +1,15 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+scalar DateTime
+
 type Note {
   id: ID
   content: String!
   author: String!
   disabled: Boolean!
+  createdAt: DateTime!
+  updatedAt: DateTime!
 }
 
 type Query {
